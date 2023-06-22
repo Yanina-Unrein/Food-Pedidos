a3151839cc4460882a1684c04a218f0d576a9f9f
Yanina <unrein.syanina@gamil.com>
2023-06-22 14:55:36 -0300

    template y estilos a componente header

---
+++ food_pedidos/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+/* const routes: Routes = [
+  { path: '', component:  },
+  { path: '', component:  }
+]; */
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
