f0002dd440ee06f7ece6c770b8b5e31cb93feed5
Yanina <unrein.syanina@gamil.com>
2023-07-06 11:57:30 -0300

    componente navbar

---
+++ food_pedidos/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  toggleButton: HTMLElement | null | undefined;
+  navWrapper: HTMLElement | null | undefined;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.toggleButton = document.getElementById('button-menu');
+    this.navWrapper = document.getElementById('nav');
+
+    if (this.toggleButton && this.navWrapper) {
+      this.toggleButton.addEventListener('click', () => {
+        this.toggleButton!.classList.toggle('close');
+        this.navWrapper!.classList.toggle('show');
+      });
+
+      this.navWrapper.addEventListener('click', (e: Event) => {
+        const target = e.target as HTMLElement;
+        if (target.id === 'nav') {
+          this.navWrapper!.classList.remove('show');
+          this.toggleButton!.classList.remove('close');
+        }
+      });
+    }
+  }
+}
