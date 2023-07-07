6354284b970a21ac7af75a57e6a7932e76c02e51
Yanina <unrein.syanina@gamil.com>
2023-07-07 12:40:30 -0300

    se modifico la version web del navbar, pequeños cambios a header

--- food_pedidos/src/app/app.module.ts
+++ food_pedidos/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from "./shared/shared.module";
@@ -14,6 +14,7 @@ import { SharedModule } from "./shared/shared.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
+        NgxSpinnerModule,
         SharedModule
     ]
 })
